6891cb9ec71578582b5d0dc37544b98f8bc6731d
Doncho N. Gunchev <dgunchev@gmail.com>
2026-03-31 00:33:13 +0300

    Start 0.3.4-dev

--- src/bluelock/__init__.py
+++ src/bluelock/__init__.py
@@ -1,5 +1,5 @@
 """BlueLock — lock and unlock your KDE session based on Bluetooth device proximity."""
 
-__version__ = "0.3.3"
+__version__ = "0.3.4-dev"
 __author__ = "Doncho Nikolaev Gunchev"
 __license__ = "Unlicense"
